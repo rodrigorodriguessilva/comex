5f76c4f8c1c2f29819164a4f7a08bd59e7ead5fc
Rodrigo <Rodrigo@senac>
2024-04-22 23:42:15 -0300

    Criação da classe Endereco

---
+++ src/main/java/br/com/alura/comex/Endereco.java
@@ -0,0 +1,27 @@
+package br.com.alura.comex;
+
+public class Endereco {
+
+
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+
+
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "rua='" + rua + '\'' +
+                ", numero='" + numero + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
