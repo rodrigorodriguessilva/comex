a791ace6d94941eaf4fc06ecef00ac7039a6dc51
Rodrigo <Rodrigo@senac>
2024-04-22 23:36:37 -0300

    Criação da classe produto

---
+++ src/main/java/br/com/alura/comex/Produto.java
@@ -0,0 +1,67 @@
+package br.com.alura.comex;
+
+import java.util.Objects;
+
+public class Produto {
+    private String nome;
+    private String descricao;
+    private   Double precoUnitario;
+    private int quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(Double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", precoUntario=" + precoUnitario +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(nome, produto.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao);
+    }
+
+
+}
